0a5dd9d1da4b9be38ccb2fdb7776ead27d79f9b7
HelgeCPH <ropf@itu.dk>
2022-06-30 13:34:09 +0200

    Added missing dependency to pyyaml module

--- setup.py
+++ setup.py
@@ -3,31 +3,29 @@ import cypher_kernel
 from setuptools import setup
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
 setup(
-    name='cypher_kernel',
+    name="cypher_kernel",
     version=cypher_kernel.__version__,
-    packages=['cypher_kernel'],
-    description='A Cypher kernel for Jupyter',
+    packages=["cypher_kernel"],
+    description="A Cypher kernel for Jupyter",
     long_description=readme,
-    author='HelgeCPH',
-    author_email='rhp@cphbusiness.dk',
-    url='https://github.com/HelgeCPH/cypher_kernel',
-    install_requires=[
-        'jupyter_client==5.2.2', 'IPython', 'ipykernel', 'requests', 'jinja2'
-    ],
+    author="HelgeCPH",
+    author_email="ropf@itu.dk",
+    url="https://github.com/HelgeCPH/cypher_kernel",
+    install_requires=["jupyter_client==5.2.2", "IPython", "ipykernel", "requests", "jinja2", "pyyaml"],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Education',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
     ],
 )
